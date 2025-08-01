4a92b45f0308c72891890086505c25df02649c2d
SHUQIAN YANG <yangsq1998@outlook.com>
2025-08-01 20:24:59 +0800

    realtime_chart

--- realtime_chart_app.py
+++ realtime_chart_app.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 
 from realtime_chart.get_realtime_data import get_realtime_data
 from realtime_chart.get_location_data import get_location_data
-from realtime_chart.get_behavior_data import get_behavior_data
 from realtime_chart.get_timeseries_data import get_time_series_data
 # from realtime_chart.sql_query import query_sql
 
@@ -37,28 +36,6 @@ def api_timeseries_data():
     """提供时间序列数据API"""
     return jsonify(get_time_series_data())
 
-# @app.route("/api/custom-query", methods=["POST"])
-# def custom_query():
-#     """
-#     自定义查询API
-#     """
-#     try:
-#         request_data = request.get_json()
-#         if not request_data or "query" not in request_data:
-#             return jsonify({
-#                 "status": "error", 
-#                 "message": "请求体中缺少 'query' 字段"
-#             }), 400
-        
-#         query = request_data["query"]
-#         result = query_sql(query)
-#         return jsonify(result)
-        
-#     except Exception as e:
-#         return jsonify({
-#             "status": "error",
-#             "message": f"查询错误: {str(e)}"
-#         }), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5003, debug=True)
